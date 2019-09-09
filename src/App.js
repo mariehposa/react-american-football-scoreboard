c5569c7af1e3d3bece4596549403d8bdff81fece
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-09 14:59:12 +0100

    Extract scoreboards as a function

--- src/App.js
+++ src/App.js
@@ -13,18 +13,9 @@ function App() {
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
+         <Club place="home" clubName = "Lions" score={homeScore}/>
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+         <Club place="away" clubName="Tigers" score={awayScore}/>
         </div>
         <BottomRow />
       </section>
@@ -49,6 +40,19 @@ function App() {
       </section>
     </div>
   );
+}
+
+
+function Club(props){
+  return (
+    <div className={props.place}>
+    <h2 className={`${props.place}__name`}>{props.clubName}</h2>
+
+    {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+    <div className={`${props.place}__score`}>{props.score}</div>
+  </div>
+  )
 }
 
 export default App;
