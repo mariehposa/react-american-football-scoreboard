05b5750860b2af7a7449436b13bd6c71af3ffc92
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-09 14:45:56 +0100

    Add functionality to buttons

--- src/App.js
+++ src/App.js
@@ -31,12 +31,20 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => {}}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => {}}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => {
+            setHomeScore(homeScore + 7)
+          }}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => {
+            setHomeScore(homeScore + 3)
+          }}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => {}}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => {}}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => {
+            setAwayScore(awayScore + 7)
+          }}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {
+            setAwayScore(awayScore + 3)
+          }}>Away Field Goal</button>
         </div>
       </section>
     </div>
